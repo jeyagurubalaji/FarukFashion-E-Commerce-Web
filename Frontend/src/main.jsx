76ebc2f9d978fc865645f32a309b7a42119e7b77
jeyagurubalaji <jeyagurubalaji@gmail.com>
2026-09-11 13:50:14 +0530

    Add main application entry point to render the React DOM

---
+++ Frontend/src/main.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+import App from './App'
+import { AuthProvider } from './context/AuthContext'
+import { CartProvider } from './context/CartContext'
+import './styles/global.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <AuthProvider>
+        <CartProvider>
+          <App />
+          <ToastContainer position="top-right" theme="dark" autoClose={3000} />
+        </CartProvider>
+      </AuthProvider>
+    </BrowserRouter>
+  </React.StrictMode>
+)
